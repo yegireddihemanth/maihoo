5de7f1b629d9da4687fed9f57fc961e5f9f8a151
yegireddihemanth <84504741+yegireddihemanth@users.noreply.github.com>
2025-10-25 21:06:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -524,7 +524,8 @@ origins = [
     "https://bab4f4a54b2b.ngrok-free.app",
     "http://10.12.88.79:5001",
     "https://2440df7ab360.ngrok-free.app",
-    "https://bab4f4a54b2b.ngrok-free.app"
+    "https://bab4f4a54b2b.ngrok-free.app",
+    "*"
 ]
 
 app.add_middleware(
