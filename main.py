f0bdb7bd07efc47b75351f2be70db8d37dafe91b
pylens <hemanthyegireddyad@gmail.com>
2025-10-26 19:38:30 +0530

    added

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from bson import ObjectId
 from fastapi.encoders import jsonable_encoder
 
 # -------------------------------
-# Config here
+# Config here 
 # -------------------------------
 mongoUri = "mongodb+srv://maihoo:akonpopStar%40143@maihoo.ztaytqd.mongodb.net/?appName=maihoo"
 mongoDbName = "bgv_core"
